cc6d22df78f79a3e0f9b67b230d1336508937880
Mikhail Agarkov <mikhail_agarkov@epam.com>
2019-11-07 16:45:24 +0300

    t2 babel and jest config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\\.js$/,
                 include: [path.resolve(__dirname, 'src')],
                 loader: 'babel-loader',
 
