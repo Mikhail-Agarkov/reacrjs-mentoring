0312393365ee7827f106c346d8079de3bf629b2c
Mikhail Agarkov <mikhail_agarkov@epam.com>
2019-11-09 18:36:08 +0300

    t2 improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: './src/index.js',
@@ -10,12 +14,21 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
 
-    plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        ...(isDev ? [] : [new MiniCssExtractPlugin()])
+    ],
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
 
     module: {
         rules: [
             {
-                test: /\\.js$/,
+                test: /\.(js|jsx)$/,
                 include: [path.resolve(__dirname, 'src')],
                 loader: 'babel-loader',
 
@@ -31,7 +44,16 @@ module.exports = {
                         ]
                     ]
                 }
-            }
+            },
+            {
+                test: /\.less$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
         ]
     }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
 const merge = require('webpack-merge');
 const config = require('./webpack.config');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(config, {
-    mode: 'production'
+    mode: 'production',
+
+    optimization: {
+        minimizer: [new UglifyJsPlugin()]
+    }
 });
