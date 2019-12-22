1a1bf2674ae773872eac9abffddeadbd672d8d52
Mikhail Agarkov <mikhail_agarkov@epam.com>
2019-12-22 23:04:52 +0300

    T8 - add jss edu example

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import styles from './status.module.scss'
+import {createUseStyles} from 'react-jss'
 
-const StatusBar = (props) => (
-    <section className={styles.content}>
-        {props.children}
-    </section>
-);
+const styles = createUseStyles({
+    content: {
+        height: 70,
+        backgroundColor: '#555555',
+        overflow: 'hidden',
+        padding: '0 40px'
+    }
+});
+
+const StatusBar = ({children}) => {
+    const classes = styles();
+
+    return (<section className={classes.content}>
+        {children}
+    </section>);
+};
+
 export default StatusBar;
